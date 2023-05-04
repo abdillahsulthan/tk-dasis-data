b22c65eb673bee74451fd4539ee9e3da2d11eee1
abdillahsulthan12 <abdillahsulthan12@gmail.com>
2023-05-05 01:46:38 +0700

    fixing-authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import show_login, show_register
+from authentication.views import show_homepage,show_login, show_register
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('', show_login, name='show_login'),
+    path('', show_homepage, name='show_homepage'),
+    path('login/', show_login, name='show_login'),
     path('register/', show_register, name='show_register' ),
 ]
