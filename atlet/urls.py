963b41955bd1647eca684690bff6bd8e3a779e0c
MarcSalvadore <marc.salvadore@ui.ac.id>
2023-05-08 23:10:12 +0700

    Bagian 3

--- atlet/urls.py
+++ atlet/urls.py
@@ -6,5 +6,10 @@ app_name = 'atlet'
 urlpatterns = [
     path('dashboard/', dashboard_atlet, name='dashboard_atlet'),
     path('enrolled_event/', enrolled_event, name='enrolled_event'),
-    path('daftar_sponsor/', daftar_sponsor, name='daftar_sponsor')
+    path('daftar_sponsor/', daftar_sponsor, name='daftar_sponsor'),
+    path('input_data_kualifikasi/', input_data_kualifikasi, name='input_data_kualifikasi'),
+    path('kategori_partner/', kategori_partner, name='kategori_partner'),
+    path('daftar_event1/', daftar_event1, name='daftar_event1'),
+    path('daftar_event2/', daftar_event2, name='daftar_event2'),
+    path('pertanyaan_kualifikasi/', pertanyaan_kualifikasi, name='pertanyaan_kualifikasi')
 ]
--- atlet/views.py
+++ atlet/views.py
@@ -9,3 +9,18 @@ def enrolled_event(request):
 
 def daftar_sponsor(request):
     return render(request, "daftar_sponsor.html")
+
+def daftar_event1(request):
+    return render(request, "daftar_event1.html")
+
+def daftar_event2(request):
+    return render(request, "daftar_event1.html")
+
+def pertanyaan_kualifikasi(request):
+    return render(request, "pertanyaan_kualifikasi.html")
+
+def input_data_kualifikasi(request):
+    return render(request, "input_data_kualifikasi.html")
+
+def kategori_partner(request):
+    return render(request, "kategori_partner.html")
